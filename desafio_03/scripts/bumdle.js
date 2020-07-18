15c209e30cf7780f2cdea0e3f6667109c167dd35
Pedro Leonardo <pedrolts16@gmail.com>
2020-07-18 02:11:38 -0300

    Commitando codigo pronto e comentado

--- desafio_03/scripts/bumdle.js
+++ desafio_03/scripts/bumdle.js
@@ -1,10 +1,16 @@
 "use strict";
 
+var _usuario;
+
 var usuario = {
   nome: "Pedro",
   idade: 19
 };
-var nome = usuario.nome,
-    idade = usuario.idade;
-console.log(nome);
-console.log(idade);
+
+function mostraInfo(usuario) {
+  // template literals permite inserir variaveis dentro da string sem necessidade de concatenacoes
+  console.log("".concat(nome, " tem ").concat(idade, " anos"));
+} // desestruturar o objeto permite acessar propridades mais facilmente
+
+
+mostraInfo((_usuario = usuario, nome = _usuario.nome, idade = _usuario.idade, _usuario));
--- desafio_03/scripts/script.js
+++ desafio_03/scripts/script.js
@@ -2,6 +2,10 @@ const usuario = {
     nome: "Pedro",
     idade: 19
 };
-const {nome, idade} = usuario;
-console.log(nome);
-console.log(idade);
+
+function mostraInfo(usuario){
+    // template literals permite inserir variaveis dentro da string sem necessidade de concatenacoes
+    console.log(`${nome} tem ${idade} anos`);
+}
+// desestruturar o objeto permite acessar propridades mais facilmente
+mostraInfo({nome,idade} = usuario);
