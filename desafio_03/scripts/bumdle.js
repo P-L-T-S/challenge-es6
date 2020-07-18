249a0006846a3e3bdf96cdffbd3572a8d00747b7
Pedro Leonardo <pedrolts16@gmail.com>
2020-07-18 02:02:11 -0300

    Publicando terceiro desafio

---
+++ desafio_03/scripts/bumdle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var usuario = {
+  nome: "Pedro",
+  idade: 19
+};
+var nome = usuario.nome,
+    idade = usuario.idade;
+console.log(nome);
+console.log(idade);
---
+++ desafio_03/scripts/script.js
@@ -0,0 +1,7 @@
+const usuario = {
+    nome: "Pedro",
+    idade: 19
+};
+const {nome, idade} = usuario;
+console.log(nome);
+console.log(idade);
