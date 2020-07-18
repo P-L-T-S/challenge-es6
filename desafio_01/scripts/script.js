de77e1f7e0ab4ac4bf05d5de1d46511df68a1d49
Pedro Leonardo <pedrolts16@gmail.com>
2020-07-17 22:40:19 -0300

    comentando codigo

--- desafio_01/scripts/script.js
+++ desafio_01/scripts/script.js
@@ -1,4 +1,6 @@
+// cria a classe
 class usuario {
+    // inicia a classe com seus valores
     constructor(nome, idade){
         this.user = {
             nome: nome,
@@ -6,20 +8,26 @@ class usuario {
             admin: ""
         };
     };
+    // metodo que retorna se usuario e adm ou nao
     isAdmin(){
         return this.user.admin === true;
     };
 };
-
+// cria classe e faz com que ela herde classe usuario
 class administrator extends usuario{
+    // inicia classe
     constructor(){
+        // chama construtor da classe pai
         super();
+        // modifica valor do atributo admin
         this.user.admin = true;
     };
 };
-
+// instancia novo usuario
 const user = new usuario;
+//instancia novo adm
 const adm = new administrator;
-
+// exibe no console se user e adm ou nao
 console.log(user.isAdmin());
+// exibe no console se user e adm ou nao
 console.log(adm.isAdmin());
