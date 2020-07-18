3ab213b56b5b42bfb9fed5c2d2b522429d42bf25
Pedro Leonardo <pedrolts16@gmail.com>
2020-07-18 01:24:30 -0300

    codigo comentado e finalizado

--- desafio_02/scripts/bundle.js
+++ desafio_02/scripts/bundle.js
@@ -1,19 +1,32 @@
 "use strict";
 
+// array a ser percorrido
 var users = [{
   nome: "Rogerio",
   idade: 18,
   curso: "Ciencia da Computacao"
 }, {
   nome: "Sabrina",
-  Idade: 17,
+  idade: 17,
   curso: "Seguranca da informacao"
 }, {
   nome: "Pedro",
   idade: 19,
-  curso: "desenvolvimento de sistemas"
-}];
+  curso: "Desenvolvimento de sistemas"
+}]; // metodo map percorre todos os itens do array
+
 var idades = users.map(function (item) {
-  return item.Idade;
+  return item.idade;
 });
-console.log(idades);
+console.log(idades); // metodo filter percorre o array e cria um  novo array com os itens que tiverem um retorno verdadeiro
+
+var adultos = users.filter(function (item) {
+  return item.idade >= 18;
+});
+console.log(adultos); // find percorre o array e retorna um valor caso encontre um item correspondente
+// O item tem q ser exatamente igual para o find encontrar
+
+var procuraCurso = users.find(function (item) {
+  return item.curso === "Seguranca da informacao";
+});
+console.log(procuraCurso);
--- desafio_02/scripts/script.js
+++ desafio_02/scripts/script.js
@@ -1,3 +1,4 @@
+// array a ser percorrido
 const users = [
     {
         nome: "Rogerio",
@@ -6,16 +7,22 @@ const users = [
     },
     {
         nome: "Sabrina",
-        Idade: 17,
+        idade: 17,
         curso: "Seguranca da informacao"
     },
     {
         nome: "Pedro",
         idade: 19,
-        curso: "desenvolvimento de sistemas"
+        curso: "Desenvolvimento de sistemas"
     }
 ];
-const idades = users.map(item => {
-    return item.Idade;
-});
+// metodo map percorre todos os itens do array
+const idades = users.map(item => item.idade);
 console.log(idades);
+// metodo filter percorre o array e cria um  novo array com os itens que tiverem um retorno verdadeiro
+const adultos = users.filter(item => item.idade >= 18);
+console.log(adultos);
+// find percorre o array e retorna um valor caso encontre um item correspondente
+// O item tem q ser exatamente igual para o find encontrar
+const procuraCurso = users.find(item => item.curso === "Seguranca da informacao");
+console.log(procuraCurso);
