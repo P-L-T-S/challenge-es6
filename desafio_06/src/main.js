d92eb3c87a49b866fac68a9bdd520706f0ad7f2e
Pedro Leonardo <pedrolts16@gmail.com>
2020-07-21 18:44:32 -0300

    progredindo nos estudos

--- desafio_06/src/main.js
+++ desafio_06/src/main.js
@@ -1,1 +1,14 @@
-alert("iae");
+const minhaPromisse = () => new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("ok");
+    }, 2000);
+});
+
+async function executaPromise(){
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+};
+
+executaPromise();
