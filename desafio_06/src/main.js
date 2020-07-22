4459b0642245f285460a7a560975056a3f66df4f
Pedro Leonardo <pedrolts16@gmail.com>
2020-07-22 03:03:34 -0300

    Usando async/await para consulta api-github

--- desafio_06/src/main.js
+++ desafio_06/src/main.js
@@ -1,27 +1,20 @@
-// cria uma nova promessa
-function minhaPromisse(x) {
-    return new Promise(resolve => {
-        // adiciona uma tempo para executar o retorno da promessa
-        setTimeout(() => {
-            // retorna a promessa
-            resolve(x);
-        }, 1000);
-    })
-};
-// cria uma funcoa assincrona que executa a promessa
-async function funcaoAssinc() {
-    // aguarda o retorno da promessa1 para ser executado
-    const response1 = await minhaPromisse("1s");
-    // apos o retorno da promessa exibe no console.log
-    console.log(response1);
-    // agurada o retorno da promessa 2
-    const response2 = await minhaPromisse("2s");
-    // apos o retorno da promessa2 para exibir no consolo.log
-    console.log(response2);
-    // aguarda o retorno da promessa 3
-    const response3 = await minhaPromisse("3s");
-    // apos o retorno da promessa 3 exibe no console.log
-    console.log(response3);
-};
-// executa a funcao assincrona
-funcaoAssinc();
+// importa o axios para aplicacao
+import axios from "axios";
+// cria a funcao onde faremos a consulta da api
+async function Api(userName){
+    // caso a promessa retorne o valor esperado
+    try{
+        // aguarda o retorno da api para executar o resto da aplicacao
+        const response = await axios.get(`https://api.github.com/users/${userName}/repos`);
+        // seleciona item por item do valor retornado
+        response.data.map(item => {
+            // exibe a propriedade nome do item
+            console.log(item.name);
+        });
+    // caso a promessa retorne um erro
+    } catch(error){
+        // exibe um alerta com o erro
+        alert(error);
+    }
+}
+Api("maykbrito");
